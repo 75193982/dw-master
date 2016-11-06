5678f4c3e369dbb938c40f6430622fc6565c3934
xgx <xuguoxin_wow@163.com>
2016-11-06 21:22:52 +0800

    新修改 11月2日

---
+++ app/src/main/java/com/xgx/dw/upload/UploadResponse.java
@@ -0,0 +1,78 @@
+package com.xgx.dw.upload;
+
+import java.io.Serializable;
+
+public class UploadResponse implements Serializable {
+    public String apkName;
+    public String apkPath;
+    public String apkSize;
+    public String createTime;
+    public String id;
+    public String remark;
+    public String versionCode;
+    public String versionName;
+
+    public String getVersionCode() {
+        return versionCode;
+    }
+
+    public void setVersionCode(String versionCode) {
+        this.versionCode = versionCode;
+    }
+
+    public String getVersionName() {
+        return versionName;
+    }
+
+    public void setVersionName(String versionName) {
+        this.versionName = versionName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getApkName() {
+        return apkName;
+    }
+
+    public void setApkName(String apkName) {
+        this.apkName = apkName;
+    }
+
+    public String getApkSize() {
+        return apkSize;
+    }
+
+    public void setApkSize(String apkSize) {
+        this.apkSize = apkSize;
+    }
+
+    public String getApkPath() {
+        return apkPath;
+    }
+
+    public void setApkPath(String apkPath) {
+        this.apkPath = apkPath;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
